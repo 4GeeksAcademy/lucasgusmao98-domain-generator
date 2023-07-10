c0343316d360379778324de5800f88306e0dc0de
Lucas Gusmao <89878660+lucasgusmao98@users.noreply.github.com>
2023-07-10 18:10:19 +0000

    completed domain generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+  const generatedomainname=() =>{
+    let pronoun = ['the','our'];
+  let adj = ['great', 'big' ];
+  let noun = ['jogger','racoon'];
+    for(let i=0; i<pronoun.length; i++){
+      for(let a=0; a<adj.length; a++){
+        for(let r=0; r<noun.length; r++){
+          console.log(pronoun[i]+adj[a]+noun[r])
+        }
+      }
+    }
+  }
+  console.log (generatedomainname())
+
